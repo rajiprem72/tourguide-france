8e129060fba5f10deada7c47e6bb35e7534c4763
Premanandhan Narayanan <premanandhan@gmail.com>
2025-10-21 20:32:51 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 self.addEventListener("install", (e) => {
   e.waitUntil(
     caches.open("tourguide-cache").then((cache) => {
-      return cache.addAll(["./", "./index.html", "./manifest.json"]);
+      return cache.addAll(["./", "./index.html", "./manifest.json","./places.html"]);
     })
   );
 });
